b0d003d4089654c8af06f711c92082774c2f88d6
Igor1D <43789550+Igor1D@users.noreply.github.com>
2025-04-08 20:29:33 +0200

    Server - fixed the Nest can't resolve dependencies of the ActivityAttendeeService (ActivityAttendeeRepository, ?, ActivityRepository) error

--- Server/src/activity-attendee/activity-attendee.module.ts
+++ Server/src/activity-attendee/activity-attendee.module.ts
@@ -3,9 +3,15 @@ import { ActivityAttendeeService } from './activity-attendee.service';
 import { ActivityAttendeeController } from './activity-attendee.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityAttendee } from './entities/activity-attendee.entity';
+import { UserModule } from '../user/user.module';
+import { ActivitiesModule } from '../activities/activities.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ActivityAttendee])],
+  imports: [
+    TypeOrmModule.forFeature([ActivityAttendee]),
+    UserModule,
+    ActivitiesModule,
+  ],
   controllers: [ActivityAttendeeController],
   providers: [ActivityAttendeeService],
 })
